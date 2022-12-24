62808a08167d346e0e7702f2eb36960a8b0ba48d
v.storozhilov <v.storozhilov@rdp.ru>
2022-12-24 18:47:02 +0300

    loading imitation

--- newsDraft/App.js
+++ newsDraft/App.js
@@ -6,6 +6,8 @@ import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import Post from './components/Post';
 import { Text, ActivityIndicator } from 'react-native';
 
+var news = []
+
 export default function App() {
 
   const [offset, setOffset] = useState(0);
@@ -14,24 +16,53 @@ export default function App() {
   const scrollRef = useRef(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const getNews = async ()=> {
+  const effect = async () => {
     setIsLoading(true);
     let response = await fetch('http://82.146.37.120:8080/documents?period=1');
     let JSresponse = await response.json();
+    news = [...JSresponse];
     setIsLoading(false);
-
-    setPosts(prev => prev.concat(JSresponse.map((item, index)=>({title : item.title,
+    setPosts(prev => prev.concat(JSresponse.map(item=>({title : item.title,
       link : item.link,
       published : item.published,
       summary : item.ds_insight.insight,
       repost_cnt : item.repost_cnt,
-      id : index
+      id : (Math.random() + 1).toString(36)
     })).slice(5 * offset, 5 * offset + 5)));
-    setOffset(prev=>prev + 1); 
+    setOffset(prev=>prev + 1);
+  }
+
+  const getNews = async ()=> {
+    setIsLoading(true);
+    setTimeout(()=>{
+      setPosts(prev => prev.concat(news.map(item=>({title : item.title,
+        link : item.link,
+        published : item.published,
+        summary : item.ds_insight.insight,
+        repost_cnt : item.repost_cnt,
+        id : (Math.random() + 1).toString(36)
+      })).slice(5 * offset, 5 * offset + 5)));
+      setOffset(prev=>prev + 1);
+      setIsLoading(false);
+    }, 2000);
+    // setIsLoading(true);
+    // let response = await fetch('http://82.146.37.120:8080/documents?period=1');
+    // let JSresponse = await response.json();
+    // setIsLoading(false);
+
+    // setPosts(prev => prev.concat(JSresponse.map(item=>({title : item.title,
+    //   link : item.link,
+    //   published : item.published,
+    //   summary : item.ds_insight.insight,
+    //   repost_cnt : item.repost_cnt,
+    //   id : (Math.random() + 1).toString(36)
+    // })).slice(5 * offset, 5 * offset + 5)));
+    // setOffset(prev=>prev + 1);
   };
 
+
   useEffect(()=>{
-    getNews();
+    effect();
   }, []);
 
   return (
@@ -41,7 +72,6 @@ export default function App() {
         paddingTop : 30,
         backgroundColor : 'black',
         alignItems: 'center'}}>
-        {posts.length ?
             <FlatList
             onEndReached={getNews}
             scrollEnabled={filter !== null ? false : true}
@@ -53,11 +83,8 @@ export default function App() {
             data={posts}
               renderItem={({ item, index })=>(
                 <Post scrollRef={scrollRef} setPosts={setPosts} posts={posts} setFilter={setFilter} item={item} filter={filter} key={item.id} index={index}/>
-              )}
-            />
-        :
-        <Text style={{paddingTop: 100, fontSize : 15, color : 'white'}}>Пожалуйста, подождите. Новости уже загружаются</Text>}
-        {isLoading ? <ActivityIndicator/> : null}
+              )}/>
+        {isLoading ? <ActivityIndicator size="large" style={{}}/> : null}
     </View>
     </IconComponentProvider>
   );
---
+++ newsDraft/metro.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
+    resolver: {
+      sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'json'] //add here
+    },
+    
+  };
